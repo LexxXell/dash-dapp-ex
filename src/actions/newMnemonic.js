66b37e4b7188f905b5fd2f7d1954cc8c81ac3842
LexxXell <lexxxell007@gmail.com>
2024-09-12 13:10:41 +0400

    Update getMnemonic

--- src/actions/newMnemonic.js
+++ src/actions/newMnemonic.js
@@ -1,11 +1,9 @@
-// src/controllers/mnemonicController.js
 const Dash = require('dash');
 const { Mnemonic } = Dash.Core;
 
 const generateMnemonic = async () => {
     const mnemonic = new Mnemonic().toString();
 
-    // Save the mnemonic in config.json
     console.log('Mnemonic: ', mnemonic);
 };
 
